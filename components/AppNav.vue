27a383ea9dabf3acc7907a1321ecddf7d4b5d5af
Adi Prayitno <adi.prayitno@icloud.com>
2022-07-11 18:33:16 +0700

    update api new

--- components/AppNav.vue
+++ components/AppNav.vue
@@ -3,7 +3,7 @@
     <ul>
       <li>
         <nuxt-link exact to="/">
-          <app-icon></app-icon>BlogName
+          <app-icon></app-icon>adiprayitno.com
         </nuxt-link>
       </li>
       <li>
--- store/index.js
+++ store/index.js
@@ -1,4 +1,4 @@
-const siteURL = "https://css-tricks.com"
+const siteURL = "https://adiprayitno.com"
 
 export const state = () => ({
   posts: [],
